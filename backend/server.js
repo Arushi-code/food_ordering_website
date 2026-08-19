3c643aa5b629a2a783b31b16d6dc101a2dbf106d
Arushi-code <aarushijha12@gmail.com>
2026-08-19 20:12:59 +0530

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -510,7 +510,7 @@ app.post('/api/group-cart/:id/add', async (req, res) => {
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all route to serve the React app for any other requests
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
